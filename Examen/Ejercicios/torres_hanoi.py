1c8886f2d55f997fda3015ae0e74178d522ce530
lauralardies <lrodrlar@myuax.com>
2022-10-28 15:49:47 +0200

    comentarios

--- Examen/Ejercicios/torres_hanoi.py
+++ Examen/Ejercicios/torres_hanoi.py
@@ -1,8 +1,16 @@
 def mover_disco(torre_inicio, torre_destino):
-    print(f"moving disk from {torre_inicio} to {torre_destino}")
+    ''' 
+    Esta función nos imprime en pantalla el movimiento realizado de las discos 
+    '''
+
+    print('Muevo el disco desde la torre ', torre_inicio, ' a la torre', torre_destino)
 
 def TorreHanoi(n, torre_inicio, torre_destino, torre_aux):
-    if n < 1:
+    '''
+    Esta función calcula los movimientos a realizar por los sacerdotes
+    '''
+
+    if n < 1: # Entramos a este if si sólo hay un disco en nuestra torre de Hanoi
         return
     TorreHanoi(n - 1, torre_inicio, torre_aux, torre_destino)
     mover_disco(torre_inicio, torre_destino)
