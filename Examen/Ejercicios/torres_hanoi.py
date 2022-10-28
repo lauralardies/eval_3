5977e6bf4bd651e96d4f297bb6192200a429f3c9
lauralardies <lrodrlar@myuax.com>
2022-10-28 15:44:45 +0200

    torre hanoi

--- Examen/Ejercicios/torres_hanoi.py
+++ Examen/Ejercicios/torres_hanoi.py
@@ -1,3 +1,12 @@
+def mover_disco(torre_inicio, torre_destino):
+    print(f"moving disk from {torre_inicio} to {torre_destino}")
 
+def TorreHanoi(n, torre_inicio, torre_destino, torre_aux):
+    if n < 1:
+        return
+    TorreHanoi(n - 1, torre_inicio, torre_aux, torre_destino)
+    mover_disco(torre_inicio, torre_destino)
+    TorreHanoi(n - 1, torre_aux, torre_destino, torre_inicio)
 
- 
+if __name__ == "__main__":
+    TorreHanoi(64, 'A', 'B', 'C')
